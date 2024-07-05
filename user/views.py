54138e00838d771dd84622d2dd58ccc08688df1f
Anatoliy Pilipey <pilipeytolya@gmail.com>
2024-07-05 18:15:29 +0300

    Add in views.py CreateUserView

--- user/views.py
+++ user/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
 
 # Create your views here.
