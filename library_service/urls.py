1ff3717140890af00ebe895e2bf0161240429818
Anatoliy Pilipey <pilipeytolya@gmail.com>
2024-07-06 10:42:33 +0300

    Registry BoorViewSer & BorrowingViewSer in router

--- library_service/urls.py
+++ library_service/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
+from library_service.views import (
+    BookViewSet,
+    BorrowingViewSet,
+)
 
 
 router = routers.DefaultRouter()
+router.registry("book", BookViewSet)
+router.registry("borrowing", BorrowingViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
