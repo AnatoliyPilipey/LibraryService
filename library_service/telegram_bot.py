c01c9353cafacfefcef41d0f99ae880d49b2772a
Anatoliy Pilipey <pilipeytolya@gmail.com>
2024-07-09 13:14:50 +0300

    For telegram bot create async manager

--- library_service/telegram_bot.py
+++ library_service/telegram_bot.py
@@ -1,19 +1,20 @@
 import os
 import asyncio
+import aiogram
+
 from telegram import Bot
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
 
 BOT_API_TOKEN = os.getenv("BOT_API_TOKEN")
-bot = Bot(token=BOT_API_TOKEN)
 
 
 async def async_send_message(message, chat_id=1053260869):
-    await bot.send_message(chat_id=chat_id, text=message)
+    async with aiogram.Bot(token=BOT_API_TOKEN) as bot:
+        await bot.send_message(chat_id=chat_id, text=message)
 
 
-def send_telegram_message(message):
-    asyncio.run(async_send_message(message))
+def send_telegram_message(message, *args, **kwargs):
+    asyncio.run(async_send_message(message, *args, **kwargs))
