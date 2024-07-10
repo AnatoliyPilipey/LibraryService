6f35bd9d4ea203cab0c8e0f50eebab4a0dc4b4a6
Anatoliy Pilipey <pilipeytolya@gmail.com>
2024-07-10 21:35:48 +0300

    Add in task.py message no overdue

--- library_service/tasks.py
+++ library_service/tasks.py
@@ -9,6 +9,8 @@ def daily_task():
     today = date.today()
     borrowing = Borrowing.objects.filter(actual_return__isnull=True)
     borrowing = borrowing.filter(expected_return__lte=today)
+    if len(borrowing) < 1:
+        send_telegram_message("No borrowings overdue today!")
     for borrow in borrowing:
         send_telegram_message(
             f"Book_id:{borrow.book_id} user_id:{borrow.user_id} expected_return:{borrow.expected_return}"
