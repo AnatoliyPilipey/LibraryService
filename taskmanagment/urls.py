1b9be86651834330a21ab144f202b7129c2596d3
Anatoliy Pilipey <pilipeytolya@gmail.com>
2024-07-13 06:50:05 +0300

    Add doc for swagger include filter for action list

--- taskmanagment/urls.py
+++ taskmanagment/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularRedocView, SpectacularAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,5 +27,16 @@ urlpatterns = [
         namespace="library_service"
     )),
     path("api/user/", include("user.urls", namespace="user")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path(
+      "api/doc/swagger/",
+      SpectacularSwaggerView.as_view(url_name="schema"),
+      name="swagger-ui",
+    ),
+    path(
+      "api/doc/redoc/",
+      SpectacularRedocView.as_view(url_name="schema"),
+      name="redoc",
+    ),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
